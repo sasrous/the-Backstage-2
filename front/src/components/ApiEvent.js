6497724d11bcacd930eda6d173424088dab05f2b
sasrous <n.casascrous@gmail.com>
2018-10-20 14:11:55 +0200

    event list working

--- front/src/components/ApiEvent.js
+++ front/src/components/ApiEvent.js
@@ -3,11 +3,24 @@ import React, { Component } from 'react';
 
 class ApiList extends Component {
   render() {
-    const eventname = this.props.info[0].displayName
- 
-
+    const eventname = this.props.info[0].performance[0].displayName
+    const date = this.props.info[0].start.date
+    const location = this.props.info[0].location.city
+    const venue = this.props.info[0].venue.displayName
+    const uri = this.props.info[0].uri
     return(
-     <p>{eventname}</p>
+      <div class="list-group">
+      
+      <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{eventname} at {venue}</h5>
+          <big class="text-muted">{date}</big>
+        </div>
+        <p class="mb-1">{uri}}</p>
+        <small class="text-muted">{location}</small>
+      </a>
+
+    </div>
     )
    
   }
